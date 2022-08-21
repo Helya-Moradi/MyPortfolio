109e0ebdc8ecac8c92cdae88cd081a7d36cf5f90
helya moradi <helya.m78@gmail.com>
2022-08-21 20:35:41 +0430

    initial portfolio

---
+++ myScript.js
@@ -0,0 +1,11 @@
+
+var list = document.querySelectorAll(".list"),
+    tabs = document.querySelectorAll(".tabs");
+
+list.forEach((item, index) => item.addEventListener("click",() => {
+    tabs.forEach((tab) => tab.classList.remove("active"));
+    list.forEach((item) => item.classList.remove("active"));
+    tabs[index].classList.add("active");
+    list[index].classList.add("active");
+}
+));
